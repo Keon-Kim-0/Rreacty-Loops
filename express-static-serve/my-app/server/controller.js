afdeda3fd38006f2cbebe641ad3bd75d30ba3252
Keon <daebaari@gmail.com>
2020-11-19 00:33:30 -0500

    11/19/2020

--- express-static-serve/my-app/server/controller.js
+++ express-static-serve/my-app/server/controller.js
@@ -7,13 +7,7 @@ const pool = new Pool({
     connectionString: "postgres://xzykdxia:cFpExSXuaxYZYtHWsQLXaUzQUY4lJjXm@lallah.db.elephantsql.com:5432/xzykdxia"
 });
 
-// Adding some notes about the database here will be helpful for future you or other developers.
-// Schema for the database can be found below:
-// https://github.com/CodesmithLLC/unit-10SB-databases/blob/master/docs/images/schema.png?raw=true
 
-// We export an object that contains a property called query,
-// which is a function that returns th`e invocation of pool.query() after logging the query
-// This will be required in the controllers to be the access point to the database
 const db = {
     query: async function (text, params, callback) {
         console.log('executed query', text);
